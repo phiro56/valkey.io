eef57f715f7646232d3676ebafab6d2655eb55e5
Daniel Phillips <phiro56@gmail.com>
2025-02-05 17:50:46 -0600

    fix: truncated Supporters component code

--- src/components/home/Supporters.tsx
+++ src/components/home/Supporters.tsx
@@ -1,25 +1,29 @@
-import { Box, Container, Heading, SimpleGrid, Stack, Text, Image } from '@chakra-ui/react';
+import { Box, Container, Heading, Image, SimpleGrid, Stack, Text } from '@chakra-ui/react';
+import aivenLogo from '/src/assets/images/aiven.svg';
+import awsLogo from '/src/assets/images/aws.svg';
+import perconaLogo from '/src/assets/images/percona.svg';
+import upcloudLogo from '/src/assets/images/upcloud.svg';
 
 const supporters = [
   {
     name: 'Pepercona',
     desc: 'Provides Valkey expertise whenever you need it, offering day-to-day operational support and hands-on assistance for complex projects.',
-    logo: '/src/assets/images/percona.svg',
+    logo: perconaLogo,
   },
   {
     name: 'Aiven',
     desc: 'In-memory NoSQL database with a small footprint and big performance. It is built on open source Valkey and compatible with legacy Redis®',
-    logo: '/src/assets/images/aiven.svg',
+    logo: aivenLogo,
   },
   {
     name: 'UpCloud',
     desc: 'With UpClouds global reach, Valkey guarantees reliability and scalability, allowing developers to focus on building outstanding applications.',
-    logo: '/src/assets/images/upcloud.svg',
+    logo: upcloudLogo,
   },
   {
     name: 'Amazon Web Services',
     desc: 'It merges Valkeys speed and versatility with Amazons manageability, perfect for data-driven applications.',
-    logo: '/src/assets/images/aws.svg',
+    logo: awsLogo,
   },
 ];
 
